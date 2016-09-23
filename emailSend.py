667c7922a80a298c6063f1a1c7e724470e025cd8
Arkadiy <khorark@mail.ru>
2016-09-23 22:57:15 +0800

    Сделана отправка email сообщения пользователю

---
+++ emailSend.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+from smtplib import SMTP_SSL
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+class SendMail():
+    def __init__(self, freeSpace = ''):
+        self.freeSpace = freeSpace
+        # Параметры письма
+        self.mail_from = 'khorark@mail.ru'
+        self.mail_to = 'khorark@ya.ru'
+        self.mail_subj = 'MAS Project: Мало свободного места!'  # заголовок письма
+        # Параметры SMTP-сервера
+        self.smtp_server = 'smtp.mail.ru'
+        self.smtp_user = self.mail_from  # пользователь smtp
+        self.smtp_pwd = ''  # пароль smtp
+
+    def constructMessage(self):
+        # формирование сообщения
+        self.multi_msg = MIMEMultipart('alternative')
+        self.multi_msg['From'] = self.mail_from
+        self.multi_msg['To'] = self.mail_to
+        self.multi_msg['Subject'] = self.mail_subj
+
+        html = """\
+        <html>
+          <head></head>
+          <body>
+            <p>
+                <h3>На сервере осталось мало свободно места!</h3>
+                <p>Пожалуйста, примите какие-либо меры, или ваш сервер упадет в ближайшее время ;-( </p>
+                <p><b>Ориентировачное оставшееся место  = {} Гб</b></p>
+            </p>
+          </body>
+        </html>
+        """.format(self.freeSpace)
+
+        msgFormat = MIMEText(html, 'html')
+        self.multi_msg.attach(msgFormat)
+
+        self.sendMessageToMail()
+
+    def sendMessageToMail(self):
+        # отправка
+        smtp = SMTP_SSL()
+        smtp.connect(self.smtp_server)
+        smtp.login(self.mail_from, self.smtp_pwd)
+        smtp.sendmail(self.mail_from, self.mail_to, self.multi_msg.as_string())
+        smtp.quit()
+
+
+if __name__ == '__main__':
+    sendM = SendMail()
+    sendM.constructMessage()
+
+
+
+
+
--- hddDaemon.py
+++ hddDaemon.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import os
-import subprocess
-import re
 import platform
 import ctypes
 import time
 from daemon import runner
+from emailSend import SendMail
 
 
 class App():
@@ -16,26 +15,25 @@ class App():
         self.pidfile_path = '/tmp/foo.pid'
         self.pidfile_timeout = 5
         self.basedir = "/home/"
+        self.buf = 0
 
     def run(self):
-        print('PID запущенного процесса - {}'.format(os.getpid()))
         while True:
-            # strOut = str(subprocess.check_output("du -sk ~/workspace", shell=True))
-            # sizeFolder = re.match(r'..(\d*).', strOut).group(1)
-
             if platform.system() == 'Windows':
                 free_bytes = ctypes.c_ulonglong(0)
                 ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(folder), None, None,
                                                            ctypes.pointer(free_bytes))
                 return free_bytes.value
             else:
+                self.buf += 1
                 freeSpace = os.statvfs('/home').f_bavail * os.statvfs('/home').f_bsize
+                freeSpaceGb = round(freeSpace/1024**3, 2)
                 print('Количество свободного места = {}Кб'.format(freeSpace))
-                print('Количество свободного места = {}Гб'.format(round(freeSpace/1024**3, 2)))
+                print('Количество свободного места = {}Гб'.format(freeSpaceGb))
+                if (self.buf == 2):
+                    print('Отправка письма!')
+                    SendMail(freeSpaceGb).constructMessage()
 
-            # print('!--> ', result2.group(1))
-            # print('==> ', result)
-            # print('Размер папки = {}Кб'.format(int(sizeFolder)))
             time.sleep(10)
 
 
